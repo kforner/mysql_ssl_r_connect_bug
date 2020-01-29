81fbb95df74fb438a07a6bae513b3074e73fd5a4
Karl Forner <Karl.Forner@precisionformedicine.com>
2020-01-29 11:09:59 +0100

    fixed scripts.

--- rmysql_ssl_connect.R
+++ rmysql_ssl_connect.R
@@ -5,8 +5,8 @@ message('connecting to root account without SSL')
 db <- dbConnect(MySQL(), host = 'mysql', user = 'root', password='root')
 
 message('connecting to ssltest account without SSL (should fail)')
-try(db <- dbConnect(MySQL(), host = 'mysql', user = 'ssltest', password='tralala'))
+try(db <- dbConnect(MySQL(), host = 'mysql', user = 'ssltest', password='ssltest'))
 
 
 message('connecting to ssltest account with SSL')
-db <- dbConnect(MySQL(), host = 'mysql', user = 'ssltest', password='tralala', client.flag = 2048)
+db <- dbConnect(MySQL(), host = 'mysql', user = 'ssltest', password='ssltest', client.flag = 2048)
